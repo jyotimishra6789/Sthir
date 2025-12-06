49d7e11f20b90e1b77a22eb1788d840b7f1d739f
jyotimishra6789 <riamishra639@gmail.com>
2025-12-06 20:20:25 +0530

    added questions

---
+++ lib/questions.ts
@@ -0,0 +1,17 @@
+export const questions=[ "I felt cheerful and in good spirits today.",
+  "I felt calm and relaxed today.",
+  "I felt active and full of energy today.",
+  "I felt motivated to complete my tasks today.",
+  "I felt tense, nervous, or worried today.",
+  "I felt overwhelmed by the things I had to do today.",
+  "I found it difficult to focus on my tasks today.",
+  "I felt down, sad, or low today.",
+  "I woke up feeling rested and refreshed today.",
+  "I felt physically healthy and balanced today."];
+  export const options=[{ value: 5, label: "All of the time" },
+  { value: 4, label: "Most of the time" },
+  { value: 3, label: "More than half of the time" },
+  { value: 2, label: "Less than half of the time" },
+  { value: 1, label: "Some of the time" },
+  { value: 0, label: "At no time" }
+];
