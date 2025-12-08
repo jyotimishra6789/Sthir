70e90868ba79c3bc34839169105d0e8ce8b42ac0
jyotimishra6789 <riamishra639@gmail.com>
2025-12-08 17:33:23 +0530

    added dashboard on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import WellnessCard from "@/components/WellnessCard";
-
+import DashboardPage from "./dashboard/page";
 export default function Home() {
   return (
     <div className="min-h-screen p-6 bg-gradient-to-br from-green-100 to-blue-100">
       <WellnessCard/>
+      <DashboardPage/>
     </div>
   );
 }
