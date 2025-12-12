5a6424e9ada9e92f190f9acc128262ae7ba98d8c
jyotimishra6789 <riamishra639@gmail.com>
2025-12-12 20:33:03 +0530

    dashboard update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import WellnessCard from "@/components/WellnessCard";
 import DashboardPage from "./dashboard/page";
+import MoodSelector from "@/components/MoodSelector";
 export default function Home() {
   return (
     <div className="min-h-screen p-6 bg-gradient-to-br from-green-100 to-blue-100">
       <WellnessCard/>
       <DashboardPage/>
+      <MoodSelector/>
     </div>
   );
 }
