c35967fb188981a2f57afd401e738a3c19d48b54
jyotimishra6789 <riamishra639@gmail.com>
2025-12-22 15:49:36 +0530

    loader fixed

--- app/loader-wrapper.tsx
+++ app/loader-wrapper.tsx
@@ -26,7 +26,7 @@ export default function LoaderWrapper({
         <img
         className="w-40 h-40 mb-4"
 
-          src="https://tenor.com/en-IN/view/thoughts-are-not-facts-mtv-mental-health-mental-health-action-day-patience-gif-21612445"
+          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMW01a2hhMHo0ZnF4dHpmZDhtamw0Zndramc3bGJnM3RuZXN0d2FoZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0tcm0qD0k4ev6Qiln1/giphy.gif"
           alt="Loading..."
         
         />
