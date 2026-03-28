b531c7686327fb8e578d2c015ac517558a691fe7
jyotimishra6789 <riamishra639@gmail.com>
2026-03-28 23:13:56 +0530

    build error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
 export default withPWA({
@@ -10,5 +10,4 @@ export default withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  ...nextConfig,
-});
+})(nextConfig);
